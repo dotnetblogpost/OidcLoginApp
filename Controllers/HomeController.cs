ae7037dfe1beb4dddd6b53f38d1841c201d036c2
aravs003 <santhosh.x.aravalli.-nd@disney.com>
2019-05-05 22:45:39 -0700

    OidcLoginApp code sample

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using OidcLoginApp.Models;
+
+namespace OidcLoginApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// login challenge to OIDC provider
+        /// </summary>
+        [HttpGet]
+        public Task Challenge(string provider, string returnUrl)
+        {
+            return HttpContext.ChallengeAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties()
+            {
+                RedirectUri = Url.Action("Index", "Home")
+            });
+        }
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Logout()
+        {
+            //app sign out 
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index");
+            //uncomment to force remote SSO sign out 
+            //await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
+            //{
+            //    RedirectUri = Url.Action("Index", "Home")
+            //});
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using OidcLoginApp.Helpers;
+
+namespace OidcLoginApp
+{
+    public class Startup
+    {
+        protected readonly ILogger<Startup> Logger;
+        protected readonly IHostingEnvironment HostingEnvironment;
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment, ILogger<Startup> logger)
+        {
+            Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
+            Logger = logger;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                 .AddCookie(options =>
+                 {
+                    // Cookie settings
+                     options.Cookie.HttpOnly = true;
+                     options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                     options.LoginPath = "/Home/Index"; //login page 
+                     options.SlidingExpiration = true;
+                }).AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.CallbackPath = "/OidcSignin";
+                    options.Authority = Configuration["authority"]; //OpenID provider authority URL
+                    options.ClientId = Configuration["clientid"];
+                    options.ClientSecret = Configuration["secret"];
+                    options.SignedOutRedirectUri = "/Home/Login";
+                    options.SaveTokens = true; //access tokens are stored in cookie. For real world scenarios, this is discouraged
+                    // Configure the scope
+                    options.Scope.Clear();
+                    options.Scope.Add("openid"); //scopes
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                    options.ResponseType = OpenIdConnectResponseType.IdToken; //returns only id token
+                    options.Events = new OpenIdConnectEvents()
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.StatusCode = 500;
+                            context.Response.ContentType = "text/plain";
+                            if (!HostingEnvironment.IsProduction() || !HostingEnvironment.IsStaging())
+                            {
+                                // Debug only, in production do not share exceptions with the remote host.
+                                return context.Response.WriteAsync(context.Exception.ToString());
+                            }
+                            return context.Response.WriteAsync(
+                                "An error occurred processing your authentication. please contact administrator");
+                        },
+                        //Force scheme of redirect URI (THE IMPORTANT PART) -- to overcome occasional correlation errors due to mismatched redirect_uri http scheme - which can happen because load balancers handle SSL sometimes forwarded headers don't reach 
+                        OnRedirectToIdentityProvider = redirectContext =>
+                        {
+                            Logger.LogInformation("redirecting to OpenID provider");
+                            redirectContext.ProtocolMessage.RedirectUri = redirectContext.ProtocolMessage.RedirectUri.Replace("http://", "https://", StringComparison.OrdinalIgnoreCase);
+                            return Task.CompletedTask;
+                        },
+                        //nice handler to capture any intermittent unhandled remote login failures - unable to match key errors or correlation errors
+                        OnRemoteFailure = context =>
+                        {
+                            Logger.LogError(context.Failure.Message, context.Failure.InnerException);
+                            context.HandleResponse();
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            context.Response.Redirect("/Home/Error");
+                            return Task.CompletedTask;
+                        },
+                        //fired when remote SSO session is also killed
+                        OnRedirectToIdentityProviderForSignOut = (context) =>
+                        {
+                            context.Response.Redirect(Configuration["end_session_endpoint"]); 
+                            context.HandleResponse();
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
